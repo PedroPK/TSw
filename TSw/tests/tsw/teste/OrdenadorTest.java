6d487599e945a69ed81d0d562fa2d8de857c405c
pedropk <pedropk@192.168.25.7>
2016-08-28 17:46:08 -0300

    Novos casos de Teste de Coleções ordenadas de forma crescente e Decrescente

--- TSw/tests/tsw/teste/OrdenadorTest.java
+++ TSw/tests/tsw/teste/OrdenadorTest.java
@@ -32,6 +32,81 @@ public class OrdenadorTest {
 	}
 	
 	@Test
+	public void getMaiorValorColecaoOrdenadaCrescenteTest() {
+		Collection<Double> colecao = getColecaoOrdenadaCrescente();
+		
+		try {
+			List<Double> resultado = Ordenador.getMaiorValor(colecao);
+			
+			Assert.assertEquals(new Double(40.0), resultado.get(1));
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void getMaiorValorColecaoOrdenadaDeCrescenteTest() {
+		Collection<Double> colecao = getColecaoOrdenadaDecrescente();
+		
+		try {
+			List<Double> resultado = Ordenador.getMaiorValor(colecao);
+			
+			Assert.assertEquals(new Double(40.0), resultado.get(1));
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void getMenorValorColecaoOrdenadaCrescenteTest() {
+		Collection<Double> colecao = getColecaoOrdenadaCrescente();
+		
+		try {
+			List<Double> resultado = Ordenador.getMaiorValor(colecao);
+			
+			Assert.assertEquals(new Double(20.0), resultado.get(0));
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void getMenorValorColecaoOrdenadaDecrescenteTest() {
+		Collection<Double> colecao = getColecaoOrdenadaDecrescente();
+		
+		try {
+			List<Double> resultado = Ordenador.getMaiorValor(colecao);
+			
+			Assert.assertEquals(new Double(20.0), resultado.get(0));
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+	private Collection<Double> getColecaoOrdenadaCrescente() {
+		Collection<Double> colecao = new HashSet<Double>();
+		
+		colecao.add( new Double(20.0));
+		colecao.add( new Double(22));
+		colecao.add( new Double(25.0));
+		colecao.add( new Double(30.0));
+		colecao.add( new Double(40.0));
+		return colecao;
+	}
+	
+	private Collection<Double> getColecaoOrdenadaDecrescente() {
+		Collection<Double> colecao = new HashSet<Double>();
+		
+		colecao.add( new Double(40.0));
+		colecao.add( new Double(30.0));
+		colecao.add( new Double(25.0));
+		colecao.add( new Double(22));
+		colecao.add( new Double(20.0));
+		
+		return colecao;
+	}
+	
+	@Test
 	public void testarColecaoNula() {
 		Collection<Double> colecao = null;
 		
