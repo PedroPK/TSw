fbed10455ec7e9828e9f553c78bd18e99c16ce99
pedropk <pedropk@192.168.25.7>
2016-08-28 23:37:55 -0300

    Criado exemplo de TDD com classes para definir Critérios de Desconto em Mensalidades

---
+++ TSw/src/tsw/interfaces/CriterioMensalidadesADS.java
@@ -0,0 +1,15 @@
+package tsw.interfaces;
+
+import tsw.excecoes.ParametroInvalidoException;
+
+public class CriterioMensalidadesADS implements CriterioMensalidades {
+	
+	public CriterioMensalidadesADS() {}
+	
+	@Override
+	public Double definirMensalidade(Double pNota) throws ParametroInvalidoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
