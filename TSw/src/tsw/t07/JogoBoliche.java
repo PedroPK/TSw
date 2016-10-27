2bd6896adda252d81c1e89421308c179426d2fa9
pedro.f-santos <pedro.f-santos@SF302249.sefaz.pe.gov.br>
2016-10-27 19:38:16 -0300

    Inicio da Implementação do Kata de Boliche

---
+++ TSw/src/tsw/t07/JogoBoliche.java
@@ -0,0 +1,15 @@
+package tsw.t07;
+
+public class JogoBoliche {
+	
+	private short aPontuacao;
+	private RodadaBoliche[] aRodadas = new RodadaBoliche[10];
+	private short aIndiceRodadaAtual = 0;
+	
+	public short getPontuacao() {
+		return this.aPontuacao;
+	}
+	
+	public void inserirJogada()
+	
+}
---
+++ TSw/src/tsw/t07/RodadaBoliche.java
@@ -0,0 +1,29 @@
+package tsw.t07;
+
+public class RodadaBoliche {
+	
+	private short aJogada01 = -1;
+	private short aJogada02 = -1;
+	
+	private short aPontuacaoRodada;
+	
+	public short getJogada01() {
+		return aJogada01;
+	}
+	public void setJogada01(short pJogada01) {
+		this.aJogada01 = pJogada01;
+	}
+	public short getJogada02() {
+		return aJogada02;
+	}
+	public void setJogada02(short pJogada02) {
+		this.aJogada02 = pJogada02;
+	}
+	public short getPontuacaoRodada() {
+		return aPontuacaoRodada;
+	}
+	public void setPontuacaoRodada(short pPontuacaoRodada) {
+		this.aPontuacaoRodada = pPontuacaoRodada;
+	}
+	
+}
