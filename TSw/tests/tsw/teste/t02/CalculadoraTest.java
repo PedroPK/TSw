cc287a43e68198076db467d839ca3563815bb4e4
pedropk <pedropk@192.168.25.6>
2017-03-24 21:31:36 -0300

    Criados métodos para implementação inicial dos primeiros testes da turma de TSw (2017.1)

--- TSw/tests/tsw/teste/t02/CalculadoraTest.java
+++ TSw/tests/tsw/teste/t02/CalculadoraTest.java
@@ -1,6 +1,7 @@
 package tsw.teste.t02;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import tsw.t02.Calculadora;
@@ -18,6 +19,28 @@ public class CalculadoraTest {
 	}
 	
 	@Test
+	public void dividir10por1() {
+		double resultado = this.aCalculadora.dividir(10, 1);
+		
+		assertEquals(10, resultado, 0.1);
+	}
+	
+	@Test
+	public void dividir1por10() {
+		double resultado = this.aCalculadora.dividir(1, 10);
+		
+		assertEquals(0.1, resultado, 0.1);
+	}
+	
+	@Test
+	public void dividir2ponto57por5ponto29() {
+		double resultado = this.aCalculadora.dividir(2.57, 5.29);
+		
+		assertEquals(0.48582231, resultado, 0.1);
+	}
+	
+	@Test
+	@Ignore
 	public void somarPrecisao16CasasDecimaisTest(){
 		double valor01 = 0.1;
 		double valor02 = 0.2;
@@ -28,6 +51,7 @@ public class CalculadoraTest {
 	}
 	
 	@Test
+	@Ignore
 	public void somarPrecisao17CasasDecimaisTest(){
 		double valor01 = 0.1;
 		double valor02 = 0.2;
@@ -38,6 +62,7 @@ public class CalculadoraTest {
 	}
 	
 	@Test
+	@Ignore
 	public void dividirTest() {
 		double dividendo = 1;
 		double divisor = 2;
@@ -48,6 +73,7 @@ public class CalculadoraTest {
 	}
 	
 	@Test
+	@Ignore
 	public void dividirPorZeroTest() {
 		double dividendo = 1;
 		double divisor = 0;
@@ -57,6 +83,7 @@ public class CalculadoraTest {
 	}
 	
 	@Test
+	@Ignore
 	public void dividirZeroPorZeroTest() {
 		double dividendo = 0;
 		double divisor = 0;
