2baba9a850c553035146a0b80facba8f591c73ae
Pedro <pedropk@gmail.com>
2017-06-29 19:52:51 -0300

    Mudando o uso de DataPoints para usar Objetos no lugar de tipos Primitivos

--- TSw/tests/tsw/teste/t14/TheoryTestes.java
+++ TSw/tests/tsw/teste/t14/TheoryTestes.java
@@ -1,5 +1,8 @@
 package tsw.teste.t14;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
@@ -10,12 +13,24 @@ import org.junit.runner.RunWith;
 public class TheoryTestes {
 	
 	@DataPoints
-	public static int[] getInteiros() {
-		return new int[] { -1, 0, 1, 2, 3, 5, 8, 13, 21};
+	public static List<Integer> getInteiros() {
+		List<Integer> resultado = new ArrayList<Integer>();
+		//resultado.add(Integer.valueOf("-1"));
+		resultado.add(Integer.valueOf("0"));
+		resultado.add(Integer.valueOf("1"));
+		resultado.add(Integer.valueOf("2"));
+		resultado.add(Integer.valueOf("3"));
+		resultado.add(Integer.valueOf("5"));
+		resultado.add(Integer.valueOf("8"));
+		resultado.add(Integer.valueOf("13"));
+		resultado.add(Integer.valueOf("21"));
+		//, 0, 1, 2, 3, 5, 8, 13, 21};
+		
+		return resultado;
 	}
 	
 	@Theory
-	public void test(int pPrimeiro, int pSegundo) {
+	public void test(Integer pPrimeiro, Integer pSegundo) {
 		Assert.assertTrue(pPrimeiro + pSegundo >= pPrimeiro);
 	}
 	
