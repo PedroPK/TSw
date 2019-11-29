8ad23660c49e27f8b9c9d3d3d512965be2235b3f
pedro.carlos.santos <pedro.carlos.santos@CPX-6BIIS9Q7KYX.dir.svc.accenture.com>
2019-11-29 15:20:51 -0300

    A third test implemented

--- src/test/java/tsw/t16/CalculadoraMediasMoveisTest.java
+++ src/test/java/tsw/t16/CalculadoraMediasMoveisTest.java
@@ -26,6 +26,16 @@ public class CalculadoraMediasMoveisTest {
 		return lista;
 	}
 	
+	/**
+	 * Lista de Valores:
+	 * 	-	11		12		14		18		15		13		12		16
+	 * 
+	 * Quantidade de Valores:
+	 *  - 	03
+	 *  
+	 * Resultado Esperado:
+	 *  - 13,667
+	 */
 	@Test
 	public void testCalcularMediaMovel_3UltimosValores() {
 		// Arrange
@@ -41,6 +51,16 @@ public class CalculadoraMediasMoveisTest {
 		assertEquals(expected, media);
 	}
 	
+	/**
+	 * Lista de Valores:
+	 * 	-	11		12		14		18		15		13		12		16
+	 * 
+	 * Quantidade de Valores:
+	 *  - 	04
+	 * 
+	 * Resultado Esperado:
+	 *  - 14
+	 */
 	@Test
 	public void testCalcularMediaMovel_4UltimosValores() {
 		// Arrange
@@ -53,6 +73,32 @@ public class CalculadoraMediasMoveisTest {
 		
 		//Assert
 		BigDecimal expected = new BigDecimal(14).setScale(3);
+		assertEquals(expected, media);
+	}
+	
+	/**
+	 * Lista de Valores:
+	 * 	-	11		12		14		18		15		13		12		16
+	 * 
+	 * Quantidade de Valores:
+	 *  - 	05
+	 * 
+	 * Resultado Esperado:
+	 *  - 14.800
+	 */
+	@Test
+	public void testCalcularMediaMovel_5UltimosValores() {
+		// Arrange
+		List<BigDecimal> lista = getLista();
+		CalculadoraMediasMoveisInterface calculadora = new CalculadoraMediasMoveis();
+		int quantidadeValores = 5;
+		
+		// Act
+		BigDecimal media = calculadora.calcular(lista, quantidadeValores);
+		
+		//Assert
+		BigDecimal expected = new BigDecimal(14.8).setScale(3, RoundingMode.HALF_EVEN);
+		
 		assertEquals(expected, media);
 	}
 
