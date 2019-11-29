7ad4fc59e71470199e38ca9c51e133cbacadf862
pedro.carlos.santos <pedro.carlos.santos@CPX-6BIIS9Q7KYX.dir.svc.accenture.com>
2019-11-29 14:49:52 -0300

    Second test passed, but first one got an ArithmeticException
    java.lang.ArithmeticException: Non-terminating decimal expansion; no
    exact representable decimal result.

--- src/main/java/tsw/t16/CalculadoraMediasMoveis.java
+++ src/main/java/tsw/t16/CalculadoraMediasMoveis.java
@@ -1,6 +1,8 @@
 package tsw.t16;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class CalculadoraMediasMoveis implements CalculadoraMediasMoveisInterface {
@@ -10,7 +12,27 @@ public class CalculadoraMediasMoveis implements CalculadoraMediasMoveisInterface
 	 */
 	@Override
 	public BigDecimal calcular(List<BigDecimal> pLista, int pQuantidadeValores) {
-		return new BigDecimal(13.666);
+		List<BigDecimal> lista = new ArrayList<BigDecimal>();
+		
+		int indiceInicial = pLista.size() - pQuantidadeValores;
+		
+		while ( indiceInicial < pLista.size() ) {
+			lista.add(pLista.get(indiceInicial));
+			
+			indiceInicial = indiceInicial + 1;
+		}
+		
+		BigDecimal soma = BigDecimal.ZERO;
+		
+		Iterator<BigDecimal>	iterator	=		lista.iterator();
+		while ( iterator.hasNext() ) {
+			soma = soma.add(iterator.next());
+		}
+		
+		BigDecimal resultado = soma.divide(new BigDecimal(pQuantidadeValores));
+		
+		
+		return resultado;
 	}
 
 }
