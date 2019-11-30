892d99ad375d52f856da005524db95a5a4189797
pedropk <pedropk@gmail.com>
2019-11-29 21:22:54 -0300

    Method to Calculate Days between two LocalDate's implemented and tested

--- src/main/java/tsw/teste/t17/CalculadoraDataHora.java
+++ src/main/java/tsw/teste/t17/CalculadoraDataHora.java
@@ -2,18 +2,21 @@ package tsw.teste.t17;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class CalculadoraDataHora implements CalculadoraDataHoraInterface {
 
 	@Override
 	public int calcularDiasEntre(LocalDate pData01, LocalDate pData02) {
-		// TODO Auto-generated method stub
-		return 0;
+		long response = ChronoUnit.DAYS.between(pData01, pData02);
+		
+		return (int) response;
 	}
 
 	@Override
 	public int calcularHorasEntre(LocalDateTime pDataHora01, LocalDateTime pDataHora02) {
-		// TODO Auto-generated method stub
+		long response = ChronoUnit.DAYS.between(pDataHora01, pDataHora02);
+		
 		return 0;
 	}
 
--- src/test/java/tsw/teste/t17/CalculadoraDataHoraTest.java
+++ src/test/java/tsw/teste/t17/CalculadoraDataHoraTest.java
@@ -8,7 +8,22 @@ import java.time.LocalDate;
 import org.junit.Test;
 
 public class CalculadoraDataHoraTest {
-
+	
+	@Test
+	public void testCalcularDiasEntre_01Janeiro2019_02Janeiro2019() {
+		// Arrange
+		LocalDate	primeiroJaneiro2019				=	LocalDate.of(2019, 01, 01);
+		LocalDate	trintaJaneiro2019				=	LocalDate.of(2019, 01, 02);
+		CalculadoraDataHoraInterface calculadora	=	new CalculadoraDataHora();
+		
+		// Act
+		int diasEntre = calculadora.calcularDiasEntre(primeiroJaneiro2019, trintaJaneiro2019);
+		
+		// Assert
+		int expectedResponse = 1;
+		assertEquals(expectedResponse, diasEntre);
+	}
+	
 	@Test
 	public void testCalcularDiasEntre_01Janeiro2019_30Janeiro2019() {
 		// Arranje
@@ -18,13 +33,42 @@ public class CalculadoraDataHoraTest {
 		
 		// Act
 		int diasEntre = calculadora.calcularDiasEntre(primeiroJaneiro2019, trintaJaneiro2019);
-		//Duration.between(primeiroJaneiro2019, trintaJaneiro2019);
 		
 		// Assert
 		int expectedResponse = 29;
 		assertEquals(expectedResponse, diasEntre);
 	}
-
+	
+	@Test
+	public void testCalcularDiasEntre_01Janeiro2019_31Janeiro2019() {
+		// Arranje
+		LocalDate	primeiroJaneiro2019				=	LocalDate.of(2019, 01, 01);
+		LocalDate	trintaJaneiro2019				=	LocalDate.of(2019, 01, 31);
+		CalculadoraDataHoraInterface calculadora	=	new CalculadoraDataHora();
+		
+		// Act
+		int diasEntre = calculadora.calcularDiasEntre(primeiroJaneiro2019, trintaJaneiro2019);
+		
+		// Assert
+		int expectedResponse = 30;
+		assertEquals(expectedResponse, diasEntre);
+	}
+	
+	@Test
+	public void testCalcularDiasEntre_01Janeiro2019_01Fevereiro2019() {
+		// Arranje
+		LocalDate	primeiroJaneiro2019				=	LocalDate.of(2019, 01, 01);
+		LocalDate	trintaJaneiro2019				=	LocalDate.of(2019, 02, 01);
+		CalculadoraDataHoraInterface calculadora	=	new CalculadoraDataHora();
+		
+		// Act
+		int diasEntre = calculadora.calcularDiasEntre(primeiroJaneiro2019, trintaJaneiro2019);
+		
+		// Assert
+		int expectedResponse = 31;
+		assertEquals(expectedResponse, diasEntre);
+	}
+	
 	@Test
 	public void testCalcularHorasEntre() {
 		fail("Not yet implemented");
